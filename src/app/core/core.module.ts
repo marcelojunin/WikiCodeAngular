eaa5149fbf2ad9c875ab426b2fd75942bc1cbb50
Marcelo <marcelojunin2010@hotmail.com>
2019-10-07 23:15:40 -0300

    Configuring core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
+  exports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ]
 })
 export class CoreModule { }
