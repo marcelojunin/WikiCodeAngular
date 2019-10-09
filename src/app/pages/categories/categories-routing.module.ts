ae76c17378868e1e047d5d4d0e16ae054b24dd68
Marcelo <marcelojunin2010@hotmail.com>
2019-10-08 23:23:19 -0300

    Configuring routes to category form.

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,8 +1,12 @@
+import { CategoryFormComponent } from './category-form/category-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
