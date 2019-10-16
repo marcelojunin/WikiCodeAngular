08742a5e45d8b7df8ede7787f1b232dac7df0150
Marcelo <marcelojunin2010@hotmail.com>
2019-10-15 23:34:02 -0300

    Implementing a formGroup to category and showing data in console.

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  save(): void {
+    console.log(this.formGroup.value);
+  }
+
+  private buildForm(): void {
+    this.formGroup = this.fb.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]]
+    })
+  }
 }
