97f6e5f6e7c07aacd737a03aafbbf3ff09de405f
Marcelo <marcelojunin2010@hotmail.com>
2019-11-12 23:45:06 -0300

    Removing angular material's modules.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,35 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
